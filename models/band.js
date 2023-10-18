a603fd30d813eb404fdf4ef8b257eff6760540a6
Luis Pimentel <luispimentel@Luiss-MacBook-Pro.local>
2023-10-17 19:27:57 -0500

    needed to submit

--- models/band.js
+++ models/band.js
@@ -1,38 +1,76 @@
+
 // DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
+const bands = require('express').Router()
+const db = require('../models')
+const { Band } = db 
 
-// MODEL
-class Band extends Model{}
+// FIND ALL BANDS
+bands.get('/', async (req, res) => {
+    try {
+        const foundBands = await Band.findAll()
+        res.status(200).json(foundBands)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
-Band.init({
-    band_id: { 
-        type: DataTypes.INTEGER, 
-        primaryKey: true,  
-        autoIncrement: true 
-    },
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false 
-    },
-    genre: { 
-        type: DataTypes.TEXT, 
-        allowNull: false 
-    },
-    available_start_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-    end_time: { 
-        type: DataTypes.DATE, 
-        allowNull: false 
-    },
-}, {
-    sequelize,                           
-    modelName: 'Band',
-    tableName: 'band',
-    timestamps: false
-}) 
+// FIND A SPECIFIC BAND
+bands.get('/:id', async (req, res) => {
+    try {
+        const foundBand = await Band.findOne({
+            where: { band_id: req.params.id }
+        })
+        res.status(200).json(foundBand)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE A BAND
+bands.put('/:id', async (req, res) => {
+    try {
+        const updatedBands = await Band.update(req.body, {
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A BAND
+bands.delete('/:id', async (req, res) => {
+    try {
+        const deletedBands = await Band.destroy({
+            where: {
+                band_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedBands} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 
 // EXPORT
-module.exports = Band
+module.exports = bands
--- server.js
+++ server.js
@@ -9,15 +9,17 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
+// CONTROLLERS 
+//Bands
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+//Events
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+//Stages
+const stageController = require('./controllers/stage_controller')
+app.use('/stage', stageController)
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
 
 // ROOT
 app.get('/', (req, res) => {
